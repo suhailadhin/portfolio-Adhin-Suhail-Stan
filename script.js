2e348a99d425801cbc793dfc2c5292cb303026d8
suhailadhin <stanadhin007@hotmail.fr>
2024-12-25 05:15:00 +0100

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+     // Loader functionality
+        window.addEventListener('load', () => {
+            const loader = document.getElementById('loader');
+            if (loader) {
+                loader.style.display = 'none';
+            }
+        });
